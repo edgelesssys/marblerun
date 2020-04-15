5dd00799224dced24b36170fcd4239edd57a7337
Felix <fs@edgeless.systems>
2020-04-15 12:33:01 +0200

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+## Build
+
+Run the following to update protobuf definitions.
+```bash
+go generate ./cmd
+```
--- cmd/grpc_server.go
+++ cmd/grpc_server.go
@@ -1,3 +1,5 @@
+//go:generate protoc --proto_path=.. --go_out=plugins=grpc:./ --go_opt=paths=source_relative api/coordinator.proto
+
 package main
 
 import (
