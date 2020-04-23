a93cde9eb044c34e26eaaca852426acbd9c8e1a4
Felix <fs@edgeless.systems>
2020-04-23 09:22:49 +0200

    requirements

--- manifest.go
+++ manifest.go
@@ -5,12 +5,28 @@ import (
 	"edgeless.systems/mesh/coordinator/rpc"
 )
 
+type requirements struct {
+	HigherSVNOK bool
+}
+
+// InfrastructureRequirements defines the requirements for the infrastructure
+type InfrastructureRequirements struct {
+	quote.InfrastructureProperties
+	requirements
+}
+
+// PackageRequirements defines the requirements for an enclave package
+type PackageRequirements struct {
+	quote.PackageProperties
+	requirements
+}
+
 // Manifest defines the rules of a mesh.
 type Manifest struct {
 	// Allowed enclave packages
-	Packages map[string]quote.PackageProperties
+	Packages map[string]PackageRequirements
 	// Allowed infrastructures
-	Infrastructures map[string]quote.InfrastructureProperties
+	Infrastructures map[string]InfrastructureRequirements
 	// Allowed node configurations
 	Nodes map[string]Node
 	// Authorized client x509 certificates
