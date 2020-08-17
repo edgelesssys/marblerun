bd2192df5b9918db81cd7392b3818ef053699cf2
olaf <olaf.vandenbosch@rub.de>
2020-08-17 14:02:18 +0200

    added ValidatorImpl. Does not check InfrastructureProperty yet

--- coordinator/quote/quote.go
+++ coordinator/quote/quote.go
@@ -2,6 +2,7 @@ package quote
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"errors"
 
 	enc "github.com/edgelesssys/ertgolib/ertenclave"
@@ -26,6 +27,15 @@ func NewMockValidator() *MockValidator {
 	}
 }
 
+// ValidatorImpl is a quote validator
+type ValidatorImpl struct {
+}
+
+// NewValidatorImpl .
+func NewValidatorImpl() *ValidatorImpl {
+	return &ValidatorImpl{}
+}
+
 // Validate implements the Validator interface
 // Input:
 // 		quote: received from Activate grpc call
@@ -45,6 +55,53 @@ func (m *MockValidator) Validate(quote []byte, message []byte, pp PackagePropert
 	if !ip.IsCompliant(entry.ip) {
 		return errors.New("infrastructure does not comply")
 	}
+	return nil
+}
+
+// Validate implements the Validator interface
+// Input:
+// 		quote: received from Activate grpc call
+//		message: raw cert from TLS context
+//		pkg, infra: from manifest
+func (m *ValidatorImpl) Validate(quote []byte, message []byte, pp PackageProperties, ip InfrastructureProperties) error {
+	/*
+		thought: VerifyRemoteReport probably calls IAS (could be observed)
+		does this mean we should allways use VerifyRemoteReport, even if it's
+		already clear the quote does not fit pp and ip? could leak information
+	*/
+	parsedReport, err := enc.VerifyRemoteReport(quote)
+
+	//check message. message in quote contains hash of x.509 cert
+
+	//copy from edbra::verifyReport
+	hash := sha256.Sum256(message)
+	if !bytes.Equal(parsedReport.Data[:len(hash)], hash[:]) {
+		return errors.New("report data does not match the certificate's hash")
+	}
+
+	//check if quote fits pp and ip
+	if err:=parsedReport.match(pp, ip); err!=nil{
+		return err
+	}
+	return nil
+}
+
+//matches properties from the report to given PackageProperties and InfrastructureProperties
+func (report enc.Report) match(pp PackageProperties, ip InfrastructureProperties) err {
+	switch {
+		report
+		case report.SecurityVersion != pp.ISVSVN:
+			return errors.New("SecurityVersion mismatch")
+		case !bytes.(report.UniqueID, pp.MREnclave):
+			return errors.New("UniqueID mismatch")
+		case !bytes.Equal(report.SignerID, pp.MRSigner):
+			return errors.New("SignerID mismatch")
+		case !bytes.Equal(report.ProductID, pp.ISVProdID):
+			return errors.New("ProductID mismatch")
+		case report.Debug !=pp.allowDebug:
+			return errors.New("Debug mismatch")
+	}
+	//TODO get information about infrastructure. either out of openEnclave or by manually parsing the quote?
 	return nil
 }
 
