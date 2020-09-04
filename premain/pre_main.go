d94f1734ce344251160060959d904617c30252f5
Moritz Eckert <m1gh7ym0@gmail.com>
2020-09-04 18:56:56 +0200

    Feat/pre main (#10)
    * AB#51 build (non-)enclaved coordinator
    
    * AB#124 AB#51 Rebasing cleanup and renaming of fresh 'Pod' usages
    
    * pre_main: WIP: Skeleton for  minimal pre_main in Go
    
    * AB#144 Create CSR and request Quote
    
    * AB#144 Test-driven fixes and move cert into env var
    
    * AB#144 Add basic test
    
    * AB#144 Do not check coordinator's cert in inital connection
    
    * AB#144 Import core without renaming
    
    * AB#144 Test: Check Cert after activation
    
    * AB#144 Extend test for pre_main
    
    * AB#144 Rebase renamings
    
    * AB#144 Fix rebase artifact in emain.cpp
    
    * AB#144 Fix typo
    
    * AB #144 Make pre_main a seperate Module
    
    * AB#144 Requested PR changes
    
    * AB#144 Do not use separate module for premain
    
    Co-authored-by: Tobias Horst <tobias.horst-v97@rub.de>

---
+++ premain/pre_main.go
@@ -0,0 +1,229 @@
+package premain
+
+import (
+	"context"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/edgelesssys/coordinator/coordinator/quote"
+	"github.com/edgelesssys/coordinator/coordinator/rpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// edgCoordinatorAddr: Required env variable with Coordinator addr
+const edgCoordinatorAddr string = "EDG_COORDINATOR_ADDR"
+
+// edgMarbleType: Required env variable with type of this marble
+const edgMarbleType string = "EDG_MARBLE_TYPE"
+
+// TODO: Create a central place where all certificate information is managed
+// TLS Cert orgName
+const orgName string = "Edgeless Systems GmbH"
+
+// Authenticator holds the information for authenticating with the Coordinator
+type Authenticator struct {
+	commonName string
+	orgName    string
+	privk      ed25519.PrivateKey
+	pubk       ed25519.PublicKey
+	initCert   *x509.Certificate
+	csr        *x509.CertificateRequest
+	quote      []byte
+	qi         quote.Issuer
+	marbleCert *x509.Certificate
+	params     *rpc.Parameters
+}
+
+// newAuthenticator creates a new Authenticator instance
+func newAuthenticator(orgName string, commonName string, qi quote.Issuer) (*Authenticator, error) {
+	a := &Authenticator{
+		commonName: commonName,
+		orgName:    orgName,
+		qi:         qi,
+	}
+	if err := a.generateCert(); err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+// loadTLSCreddentials builds a TLS config from the Authenticator's self-signed certificate and the Coordinator's RootCA
+func loadTLSCredentials(a *Authenticator) (credentials.TransportCredentials, error) {
+	clientCert, err := a.getTLSCertificate()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Marble self-signed x509 certificate")
+	}
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{*clientCert},
+		InsecureSkipVerify: true,
+	}
+	return credentials.NewTLS(tlsConfig), nil
+}
+
+// getTLSCertificate creates a TLS certificate for the Marbles self-signed x509 certificate
+func (a *Authenticator) getTLSCertificate() (*tls.Certificate, error) {
+	return tlsCertFromDER(a.initCert.Raw, a.privk), nil
+}
+
+// generateSerial returns a random serialNumber
+func generateSerial() (*big.Int, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	return rand.Int(rand.Reader, serialNumberLimit)
+}
+
+// generateCert generates a new self-signed certificate associated key-pair
+func (a *Authenticator) generateCert() error {
+
+	// code (including generateSerial()) adapted from golang.org/src/crypto/tls/generate_cert.go
+	pubk, privk, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+	notBefore := time.Now()
+	notAfter := notBefore.Add(math.MaxInt64)
+
+	serialNumber, err := generateSerial()
+	if err != nil {
+		return err
+	}
+
+	// TODO: what else do we need to set here?
+	// Do we need x509.KeyUsageKeyEncipherment?
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{a.orgName},
+			CommonName:   a.commonName,
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: false,
+		IsCA:                  true,
+	}
+
+	certRaw, err := x509.CreateCertificate(rand.Reader, &template, &template, pubk, privk)
+	if err != nil {
+		return err
+	}
+	cert, err := x509.ParseCertificate(certRaw)
+	if err != nil {
+		return err
+	}
+	quote, err := a.qi.Issue(certRaw)
+	if err != nil {
+		return err
+	}
+	a.pubk = pubk
+	a.privk = privk
+	a.quote = quote
+	a.initCert = cert
+	return nil
+}
+
+func (a *Authenticator) generateCSR() error {
+	template := x509.CertificateRequest{
+		Subject: pkix.Name{
+			Organization: []string{a.orgName},
+			CommonName:   a.commonName,
+		},
+		PublicKey: a.pubk,
+	}
+	csrRaw, err := x509.CreateCertificateRequest(rand.Reader, &template, a.privk)
+	if err != nil {
+		return err
+	}
+	csr, err := x509.ParseCertificateRequest(csrRaw)
+	if err != nil {
+		return err
+	}
+	a.csr = csr
+	return nil
+}
+
+// tlsCertFromDER converts a certificate from raw DER representation to a tls.Certificate
+func tlsCertFromDER(certDER []byte, privk interface{}) *tls.Certificate {
+	return &tls.Certificate{Certificate: [][]byte{certDER}, PrivateKey: privk}
+}
+
+// preMain is supposed to run before the App's actual main and authenticate with the Coordinator
+func preMain(a *Authenticator) (*x509.Certificate, *rpc.Parameters, error) {
+	// get env variables
+	coordAddr := os.Getenv(edgCoordinatorAddr)
+	if len(coordAddr) == 0 {
+		return nil, nil, fmt.Errorf("environment variable not set: %v", edgCoordinatorAddr)
+	}
+
+	marbleType := os.Getenv(edgMarbleType)
+	if len(marbleType) == 0 {
+		return nil, nil, fmt.Errorf("environment variable not set: %v", edgMarbleType)
+	}
+
+	// load TLS Credentials
+	tlsCredentials, err := loadTLSCredentials(a)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// initiate grpc connection to Coordinator
+	cc, err := grpc.Dial(coordAddr, grpc.WithTransportCredentials(tlsCredentials))
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer cc.Close()
+
+	// generate CSR
+	if err := a.generateCSR(); err != nil {
+		return nil, nil, err
+	}
+
+	// authenticate with Coordinator
+	c := rpc.NewMarbleClient(cc)
+	req := &rpc.ActivationReq{
+		CSR:        a.csr.Raw,
+		MarbleType: marbleType,
+		Quote:      a.quote,
+	}
+
+	activiationResp, err := c.Activate(context.Background(), req)
+	if err != nil {
+		return nil, nil, err
+	}
+	newCert, err := x509.ParseCertificate(activiationResp.GetCertificate())
+	if err != nil {
+		return nil, nil, err
+	}
+	a.marbleCert = newCert
+	a.params = activiationResp.GetParameters()
+
+	// TODO: Store certificate in virtual file system and call actual main with params
+	return a.marbleCert, a.params, nil
+
+}
+
+func main() {
+	commonName := "marble"          // Coordinator will assign an ID to us
+	issuer := quote.NewMockIssuer() // TODO: Use real issuer
+	a, err := newAuthenticator(orgName, commonName, issuer)
+	if err != nil {
+		log.Fatalf("failed to create Authenticator: %v", err)
+	}
+	_, _, err = preMain(a)
+	if err != nil {
+		log.Fatalf("pre_main failed: %v", err)
+	}
+}
---
+++ premain/pre_main_test.go
@@ -0,0 +1,198 @@
+package premain
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/edgelesssys/coordinator/coordinator/core"
+	"github.com/edgelesssys/coordinator/coordinator/quote"
+	"github.com/edgelesssys/coordinator/coordinator/server"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const manifestJSON string = `{
+	"Packages": {
+		"backend": {
+			"MREnclave": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+			"MiscSelect": 1111111,
+			"Attributes": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+		},
+		"frontend": {
+			"MRSigner": [31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],
+			"ISVProdID": 44,
+			"ISVSVN": 3,
+			"Attributes": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+			"MiscSelect": 1111111
+		}
+	},
+	"Infrastructures": {
+		"Azure": {
+			"QESVN": 2,
+			"PCESVN": 3,
+			"CPUSVN": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+			"RootCA": [3,3,3]
+		},
+		"Alibaba": {
+			"QESVN": 2,
+			"PCESVN": 4,
+			"CPUSVN": [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],
+			"RootCA": [4,4,4]
+		}
+	},
+	"Marbles": {
+		"backend_first": {
+			"Package": "backend",
+			"MaxActivations": 1,
+			"Parameters": {
+				"Files": {
+					"/abc/defg.txt": [7,7,7],
+					"/ghi/jkl.mno": [8,8,8]
+				},
+				"Env": {
+					"IS_FIRST": "true"
+				},
+				"Argv": [
+					"--first",
+					"serve"
+				]
+			}
+		},
+		"backend_other": {
+			"Package": "backend",
+			"Parameters": {
+				"Argv": [
+					"serve"
+				]
+			}
+		},
+		"frontend": {
+			"Package": "frontend"
+		}
+	},
+	"Clients": {
+		"owner": [9,9,9]
+	}
+}`
+
+const coordinatorCommonName string = "Coordinator" // TODO: core does not export this, for now just use it hardcoded
+
+func TestLogic(t *testing.T) {
+	assert := assert.New(t)
+
+	// parse manifest
+	var manifest core.Manifest
+	err := json.Unmarshal([]byte(manifestJSON), &manifest)
+	assert.Nil(err, err)
+	validator := quote.NewMockValidator()
+	issuer := quote.NewMockIssuer()
+
+	// create core and run gRPC server
+	coordinator, err := core.NewCore(orgName, validator, issuer)
+	assert.NotNil(coordinator, "coordinator empty")
+	assert.Nil(err, err)
+
+	coordinator.SetManifest(context.TODO(), []byte(manifestJSON))
+
+	// run marble server
+	var grpcAddr string
+	addrChan := make(chan string)
+	errChan := make(chan error)
+	marbleServerAddr := flag.String("ip", "localhost:0", "")
+	go server.RunMarbleServer(coordinator, *marbleServerAddr, addrChan, errChan)
+	select {
+	case err = <-errChan:
+		fmt.Println("Failed to start gRPC server", err)
+	case grpcAddr = <-addrChan:
+		fmt.Println("start mesh server at", grpcAddr)
+	}
+
+	spawner := marbleSpawner{
+		assert:     assert,
+		issuer:     issuer,
+		validator:  validator,
+		manifest:   manifest,
+		serverAddr: grpcAddr,
+	}
+	// activate first backend
+	spawner.newMarble("backend_first", "Azure", true)
+
+	// try to activate another first backend
+	spawner.newMarble("backend_first", "Azure", false)
+
+	// activate 10 other backend
+	pickInfra := func(i int) string {
+		if i&1 == 0 {
+			return "Azure"
+		}
+		return "Alibaba"
+	}
+	for i := 0; i < 10; i++ {
+		spawner.newMarble("backend_other", pickInfra(i), true)
+	}
+
+	// activate 10 frontend
+	for i := 0; i < 10; i++ {
+		spawner.newMarble("frontend", pickInfra(i), true)
+	}
+
+}
+
+type marbleSpawner struct {
+	manifest   core.Manifest
+	validator  *quote.MockValidator
+	issuer     quote.Issuer
+	serverAddr string
+	assert     *assert.Assertions
+}
+
+func (ms marbleSpawner) newMarble(marbleType string, infraName string, shouldSucceed bool) {
+	// set env vars
+	err := os.Setenv(edgCoordinatorAddr, ms.serverAddr)
+	ms.assert.Nil(err, "failed to set env variable: %v", err)
+	err = os.Setenv(edgMarbleType, marbleType)
+	ms.assert.Nil(err, "failed to set env variable: %v", err)
+
+	// create Authenticator
+	commonName := "marble"          // Coordinator will assign an ID to us
+	issuer := quote.NewMockIssuer() // TODO: Use real issuer
+	a, err := newAuthenticator(orgName, commonName, issuer)
+	ms.assert.Nil(err, "failed to create Authenticator: %v", err)
+	ms.assert.NotNil(a, "got empty Authenticator")
+
+	// store quote in validator
+	marble, ok := ms.manifest.Marbles[marbleType]
+	ms.assert.True(ok, "marbleType '%v' does not exist", marbleType)
+	pkg, ok := ms.manifest.Packages[marble.Package]
+	ms.assert.True(ok, "Package '%v' does not exist", marble.Package)
+	infra, ok := ms.manifest.Infrastructures[infraName]
+	ms.assert.True(ok, "Infrastructure '%v' does not exist", infraName)
+	ms.validator.AddValidQuote(a.quote, a.initCert.Raw, pkg, infra)
+
+	// call preMain
+	cert, params, err := preMain(a)
+	if !shouldSucceed {
+		ms.assert.NotNil(err, err)
+		ms.assert.Nil(cert, "expected empty cert, but got %v", cert)
+		ms.assert.Nil(params, "expected empty params, but got %v", params)
+		return
+	}
+	ms.assert.Nil(err, "preMain failed: %v", err)
+	ms.assert.NotNil(cert, "got empty cert: %v", cert)
+	ms.assert.NotNil(params, "got empty params: %v", params)
+
+	ms.assert.Equal(marble.Parameters.Files, a.params.Files, "expected equal: '%v' - '%v'", marble.Parameters.Files, a.params.Files)
+	ms.assert.Equal(marble.Parameters.Env, a.params.Env, "expected equal: '%v' - '%v'", marble.Parameters.Env, a.params.Env)
+	ms.assert.Equal(marble.Parameters.Argv, a.params.Argv, "expected equal: '%v' - '%v'", marble.Parameters.Argv, a.params.Argv)
+
+	ms.assert.Equal(a.marbleCert.Issuer.CommonName, coordinatorCommonName, "expected equal: '%v' - '%v'", a.marbleCert.Issuer.CommonName, coordinatorCommonName)
+	ms.assert.Equal(a.marbleCert.Issuer.Organization[0], orgName, "expected equal: '%v' - '%v'", a.marbleCert.Issuer.Organization[0], orgName)
+	// commonName gets overwritten
+	// assert.Equal(a.marbleCert.Subject.CommonName, a.commonName)
+	ms.assert.Equal(a.marbleCert.Subject.Organization[0], a.orgName, "expected equal: '%v' - '%v'", a.marbleCert.Subject.Organization[0], a.orgName)
+	ms.assert.Equal(a.marbleCert.PublicKey, a.pubk, "expected equal: '%v' - '%v'", a.marbleCert.PublicKey, a.pubk)
+}
