dd57f87aef0b6b56e4be7717c1fcf03a72e6a9dc
Moritz Eckert <m1gh7ym0@gmail.com>
2020-09-07 19:14:27 +0200

    AB#163 Use PreMain correctly

--- premain/cmd/marble/main.go
+++ premain/cmd/marble/main.go
@@ -1,12 +1,23 @@
 package marbletest
 
+import (
+	"github.com/edgelesssys/coordinator/coordinator/quote"
+	"github.com/edgelesssys/coordinator/premain"
+)
+
 func main() {}
 
 func premainTest() int {
 	// call preMain
-	cert, params, err := preMain(a)
+	commonName := "marble"         // Coordinator will assign an ID to us
+	issuer := quote.NewERTIssuer() // TODO: Use real issuer
+	a, err := premain.NewAuthenticator(orgName, commonName, issuer)
 	if err != nil {
 		return -1
+	}
+	_, _, err := premain.PreMain(a)
+	if err != nil {
+		return -2
 	}
 	return 0
 }
