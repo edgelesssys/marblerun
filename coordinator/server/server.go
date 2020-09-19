29858adc3d4a3ffd400588bfa5bb082f94de8cfd
olaf <olaf.vandenbosch@rub.de>
2020-09-19 13:37:48 +0200

    add server.go

--- coordinator/server/server.go
+++ coordinator/server/server.go
@@ -1,24 +1,30 @@
 package server
 
 import (
-	"context"
 	"crypto/tls"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
-	"strings"
 
 	"github.com/edgelesssys/coordinator/coordinator/core"
 	"github.com/edgelesssys/coordinator/coordinator/rpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-
-	"github.com/golang/gddo/httputil/header"
 )
+
+type certQuoteResp struct {
+	Cert  string
+	Quote []byte
+}
+type statusResp struct {
+	Status    string
+	Signature []byte
+}
+type manifestSignatureResp struct {
+	ManifestSignature []byte
+}
 
 // RunMarbleServer starts a gRPC with the given Coordinator core.
 // `address` is the desired TCP address like "localhost:0".
@@ -49,116 +55,73 @@ func RunMarbleServer(core *core.Core, addr string, addrChan chan string, errChan
 	}
 }
 
-// TODO temporary ClientAPI implementation
-type SetManifestRequest struct {
-	// Manifest []byte `json:"manifest"`
-	Manifest []byte
-}
-
-type malformedRequest struct {
-	status int
-	msg    string
-}
-
-func (mr *malformedRequest) Error() string {
-	return mr.msg
-}
-
-func decodeJSONBody(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	if r.Header.Get("Content-Type") != "" {
-		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
-		if value != "application/json" {
-			msg := "Content-Type header is not application/json"
-			return &malformedRequest{status: http.StatusUnsupportedMediaType, msg: msg}
-		}
-	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
-
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields()
-
-	err := dec.Decode(&dst)
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON")
-			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-
-		case errors.As(err, &unmarshalTypeError):
-			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
-			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-
-		case errors.Is(err, io.EOF):
-			msg := "Request body must not be empty"
-			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-
-		case err.Error() == "http: request body too large":
-			msg := "Request body must not be larger than 1MB"
-			return &malformedRequest{status: http.StatusRequestEntityTooLarge, msg: msg}
-
-		default:
-			return err
-		}
-	}
-
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		msg := "Request body must only contain a single JSON object"
-		return &malformedRequest{status: http.StatusBadRequest, msg: msg}
-	}
-
-	return nil
-}
-
 // CreateServeMux creates a mux that serves the client API. provisionally
-func CreateServeMux(c *core.Core) *http.ServeMux {
+func CreateServeMux(cc core.ClientCore) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
-		report := "hello world"
-		fmt.Println("/quote hit")
-		if len(report) == 0 {
-			http.Error(w, "failed to get quote", http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(report))
-	})
-
-	mux.HandleFunc("/set_manifest", func(w http.ResponseWriter, r *http.Request) {
-		var req SetManifestRequest
-
-		// reqBody, _ := ioutil.ReadAll(r.Body)
-		// err := json.Unmarshal(reqBody, &req)
-		err := decodeJSONBody(w, r, &req)
-		if err != nil {
-			var mr *malformedRequest
-			if errors.As(err, &mr) {
-				http.Error(w, mr.msg, mr.status)
-			} else {
-				log.Println(err.Error())
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			status, signature, err := cc.GetStatus(r.Context())
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
-			return
+			strct := statusResp{status, signature}
+			jsn, err := json.Marshal(strct)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Write(jsn)
+		default:
+			http.Error(w, "", http.StatusMethodNotAllowed)
 		}
-		// http.Error(w, "parsed successfully: req.Manifest", http.StatusInternalServerError)
-		// http.Error(w, fmt.Sprintf("parsed successfully: req.Manifest=\n%v", req.Manifest), http.StatusAccepted)
-
-		if err = c.SetManifest(context.TODO(), req.Manifest); err != nil {
-			http.Error(w, fmt.Sprintf("failed to set manifest: %v", err), http.StatusInternalServerError)
-			return
+	})
+	mux.HandleFunc("/manifest", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			signature, err := cc.GetManifestSignature(r.Context())
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			strct := manifestSignatureResp{signature}
+			jsn, err := json.Marshal(strct)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			io.WriteString(w, string(jsn))
+		case http.MethodPost:
+			manifest := r.FormValue("manifest")
+			err := cc.SetManifest(r.Context(), []byte(manifest))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			fmt.Println(manifest)
+		default:
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		}
+	})
+	mux.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			cert, quote, err := cc.GetCertQuote(r.Context())
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			strct := certQuoteResp{cert, quote}
+			jsn, err := json.Marshal(strct)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Write(jsn)
+		default:
+			http.Error(w, "", http.StatusMethodNotAllowed)
 		}
 	})
 
@@ -173,5 +136,5 @@ func RunClientServer(mux *http.ServeMux, address string, tlsConfig *tls.Config) 
 		TLSConfig: tlsConfig,
 	}
 	fmt.Println("start client server at ", address)
-	fmt.Println(server.ListenAndServe())
+	fmt.Println(server.ListenAndServeTLS("", ""))
 }
---
+++ coordinator/server/server_test.go
@@ -0,0 +1,169 @@
+package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/edgelesssys/coordinator/coordinator/core"
+	"github.com/edgelesssys/coordinator/coordinator/quote"
+	"github.com/stretchr/testify/assert"
+)
+
+const manifestJSON string = `{
+	"Packages": {
+		"backend": {
+			"UniqueID": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+			"Debug": false
+		},
+		"frontend": {
+			"SignerID": [31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],
+			"ProductID": [44],
+			"SecurityVersion": 3,
+			"Debug": true
+		}
+	},
+	"Infrastructures": {
+		"Azure": {
+			"QESVN": 2,
+			"PCESVN": 3,
+			"CPUSVN": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+			"RootCA": [3,3,3]
+		},
+		"Alibaba": {
+			"QESVN": 2,
+			"PCESVN": 4,
+			"CPUSVN": [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],
+			"RootCA": [4,4,4]
+		}
+	},
+	"Marbles": {
+		"backend_first": {
+			"Package": "backend",
+			"MaxActivations": 1,
+			"Parameters": {
+				"Files": {
+					"/abc/defg.txt": "foo",
+					"/ghi/jkl.mno": "bar"
+				},
+				"Env": {
+					"IS_FIRST": "true",
+					"ROOT_CA": "$$root_ca",
+					"SEAL_KEY": "$$seal_key",
+					"MARBLE_CERT": "$$marble_cert",
+					"MARBLE_KEY": "$$marble_key"
+				},
+				"Argv": [
+					"--first",
+					"serve"
+				]
+			}
+		},
+		"backend_other": {
+			"Package": "backend",
+			"Parameters": {
+				"Env": {
+					"ROOT_CA": "$$root_ca",
+					"SEAL_KEY": "$$seal_key",
+					"MARBLE_CERT": "$$marble_cert",
+					"MARBLE_KEY": "$$marble_key"
+				},
+				"Argv": [
+					"serve"
+				]
+			}
+		},
+		"frontend": {
+			"Package": "frontend",
+			"Parameters": {
+				"Env": {
+					"ROOT_CA": "$$root_ca",
+					"SEAL_KEY": "$$seal_key",
+					"MARBLE_CERT": "$$marble_cert",
+					"MARBLE_KEY": "$$marble_key"
+				}
+			}
+		}
+	},
+	"Clients": {
+		"owner": [9,9,9]
+	}
+}`
+
+func TestQuote(t *testing.T) {
+	assert := assert.New(t)
+	validator := quote.NewMockValidator()
+	issuer := quote.NewMockIssuer()
+	c, err := core.NewCore("edgeless", validator, issuer)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := CreateServeMux(c)
+
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:25555/quote", nil)
+	w := httptest.NewRecorder()
+
+	mux.ServeHTTP(w, req)
+	resp := w.Result()
+	assert.Equal(200, resp.StatusCode)
+
+}
+func TestManifest(t *testing.T) {
+	assert := assert.New(t)
+	validator := quote.NewMockValidator()
+	issuer := quote.NewMockIssuer()
+	c, err := core.NewCore("edgeless", validator, issuer)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := CreateServeMux(c)
+
+	//set manifest
+	form := url.Values{}
+	form.Add("manifest", manifestJSON)
+
+	req := httptest.NewRequest(http.MethodPost, "/manifest", nil)
+	req.PostForm = form
+
+	w := httptest.NewRecorder()
+
+	mux.ServeHTTP(w, req)
+	resp := w.Result()
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	//get manifest signature
+	req = httptest.NewRequest(http.MethodGet, "/manifest", nil)
+
+	w = httptest.NewRecorder()
+
+	mux.ServeHTTP(w, req)
+	resp = w.Result()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	assert.Contains(string(b), "{\"ManifestSignature\":")
+
+	//try set manifest again, should fail
+	req = httptest.NewRequest(http.MethodPost, "/manifest", nil)
+	req.PostForm = form
+	w = httptest.NewRecorder()
+
+	mux.ServeHTTP(w, req)
+
+	resp = w.Result()
+
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+	assert.Equal("server is not in expected state\n", string(b))
+
+}
