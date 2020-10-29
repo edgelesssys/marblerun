5084a8018dcbd55ffe54762a90991334f34bf703
Thomas Tendyck <tt@edgeless.systems>
2020-10-29 16:37:00 +0100

    cleanup tests

--- coordinator/server/server_test.go
+++ coordinator/server/server_test.go
@@ -1,8 +1,8 @@
 package server
 
 import (
-	"bytes"
-	"io/ioutil"
+	"context"
+	"encoding/hex"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,6 +12,7 @@ import (
 	"github.com/edgelesssys/coordinator/coordinator/core"
 	"github.com/edgelesssys/coordinator/test"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestQuote(t *testing.T) {
@@ -20,57 +21,38 @@ func TestQuote(t *testing.T) {
 	mux := CreateServeMux(core.NewCoreWithMocks())
 
 	req := httptest.NewRequest(http.MethodGet, "/quote", nil)
-	w := httptest.NewRecorder()
-
-	mux.ServeHTTP(w, req)
-	assert.Equal(http.StatusOK, w.Code)
-
+	resp := httptest.NewRecorder()
+	mux.ServeHTTP(resp, req)
+	assert.Equal(http.StatusOK, resp.Code)
 }
 
 func TestManifest(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
 	c := core.NewCoreWithMocks()
 	mux := CreateServeMux(c)
 
-	//set manifest
-	req := httptest.NewRequest(http.MethodPost, "/manifest", bytes.NewReader([]byte(test.ManifestJSON)))
+	// set manifest
+	req := httptest.NewRequest(http.MethodPost, "/manifest", strings.NewReader(test.ManifestJSON))
+	resp := httptest.NewRecorder()
+	mux.ServeHTTP(resp, req)
+	require.Equal(http.StatusOK, resp.Code)
 
-	w := httptest.NewRecorder()
-
-	mux.ServeHTTP(w, req)
-	resp := w.Result()
-	assert.Equal(http.StatusOK, resp.StatusCode)
-
-	//get manifest signature
+	// get manifest signature
 	req = httptest.NewRequest(http.MethodGet, "/manifest", nil)
+	resp = httptest.NewRecorder()
+	mux.ServeHTTP(resp, req)
+	require.Equal(http.StatusOK, resp.Code)
 
-	w = httptest.NewRecorder()
+	sig := hex.EncodeToString(c.GetManifestSignature(context.TODO()))
+	assert.JSONEq(`{"ManifestSignature":"`+sig+`"}`, resp.Body.String())
 
-	mux.ServeHTTP(w, req)
-	resp = w.Result()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	assert.Equal(http.StatusOK, resp.StatusCode)
-	assert.Contains(string(b), "{\"ManifestSignature\":")
-
-	//try set manifest again, should fail
-	req = httptest.NewRequest(http.MethodPost, "/manifest", bytes.NewReader([]byte(test.ManifestJSON)))
-	w = httptest.NewRecorder()
-
-	mux.ServeHTTP(w, req)
-
-	resp = w.Result()
-
-	b, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	assert.Equal(http.StatusBadRequest, resp.StatusCode)
-	assert.Equal("server is not in expected state\n", string(b))
+	// try setting manifest again, should fail
+	req = httptest.NewRequest(http.MethodPost, "/manifest", strings.NewReader(test.ManifestJSON))
+	resp = httptest.NewRecorder()
+	mux.ServeHTTP(resp, req)
+	require.Equal(http.StatusBadRequest, resp.Code)
 }
 
 func TestConcurrent(t *testing.T) {
