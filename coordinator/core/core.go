491bfa8c9f524128599cecc13e28241e23b43bc2
Thomas Tendyck <tt@edgeless.systems>
2020-11-19 10:02:12 +0100

    add license header to go files

--- coordinator/core/core.go
+++ coordinator/core/core.go
@@ -1,3 +1,9 @@
+// Copyright (c) Edgeless Systems GmbH.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // Package core provides the core functionality for the Coordinator object including state transition, APIs for marbles and clients, handling of manifests and the sealing functionalities.
 package core
 
