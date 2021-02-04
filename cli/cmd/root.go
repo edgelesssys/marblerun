2fcdf71aad36d1bf5fda458fd16689a838df8edd
daniel-weisse <dw@edgeless.systems>
2021-02-04 10:03:19 +0100

    AB#587 added suggested changes

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -13,10 +13,6 @@ import (
 var eraConfig string
 var insecureEra bool
 
-const eraString = `
-Path to remote attestation config file in json format, if none provided the newest configuration will be loaded from github
-`
-
 var rootCmd = &cobra.Command{
 	Use:   "marblerun",
 	Short: "marblerun cli short description",
@@ -29,54 +25,39 @@ func Execute() error {
 }
 
 func init() {
-
-	rootCmd.AddCommand(newManifestCmd())
-	rootCmd.AddCommand(newNamespaceCmd())
-	rootCmd.AddCommand(newInstallCmd())
-	rootCmd.AddCommand(newRecoverCmd())
 	rootCmd.AddCommand(newRootCACmd())
 }
 
-// VerifyCoordinator verifies the connection using era
-func VerifyCoordinator(host string, configFilename string, insecure bool) (string, error) {
-	var cert string
-	var err error
-
+// verify the connection to the marblerun coordinator
+func verifyCoordinator(host string, configFilename string, insecure bool) (string, error) {
+	// skip verification if specified
 	if insecure {
-		// skip verification if specified
 		fmt.Println("Warning: skipping quote verification")
-		cert, err = era.InsecureGetCertificate(host)
-		if err != nil {
-			return "", err
-		}
-	} else if configFilename == "" {
-		// get latest config from github if none specified
-		fmt.Println("No era config file specified, getting latest config from github.com/edgelesssys/marblerun/releases/latest/download/coordinator-era.json")
-		resp, err := http.Get("https://github.com/edgelesssys/marblerun/releases/latest/download/coordinator-era.json")
-		if err != nil {
-			return "", err
-		}
-		defer resp.Body.Close()
-		out, err := os.Create("era-config.json")
-		if err != nil {
-			return "", err
-		}
-		defer out.Close()
-		_, err = io.Copy(out, resp.Body)
-		if err != nil {
-			return "", err
-		}
-		fmt.Println("Got latest config")
-
-		cert, err = era.GetCertificate(host, "era-config.json")
-		if err != nil {
-			return "", err
-		}
-	} else {
-		cert, err = era.GetCertificate(host, configFilename)
-		if err != nil {
-			return "", err
-		}
+		return era.InsecureGetCertificate(host)
 	}
-	return cert, nil
+
+	// get certificate using provided config
+	if configFilename != "" {
+		return era.GetCertificate(host, configFilename)
+	}
+
+	// get latest config from github if none specified
+	fmt.Println("No era config file specified, getting latest config from github.com/edgelesssys/marblerun/releases/latest/download/coordinator-era.json")
+	resp, err := http.Get("https://github.com/edgelesssys/marblerun/releases/latest/download/coordinator-era.json")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	out, err := os.Create("era-config.json")
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("Got latest config")
+
+	return era.GetCertificate(host, "era-config.json")
 }
--- cli/cmd/rootCA.go
+++ cli/cmd/rootCA.go
@@ -7,9 +7,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCAFilename string
-
 func newRootCACmd() *cobra.Command {
+	var rootCAFilename string
+
 	cmd := &cobra.Command{
 		Use:   "rootca <IP:PORT>",
 		Short: "returns the rootCA of the marblerun coordinator",
@@ -17,20 +17,20 @@ func newRootCACmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			hostName := args[0]
-			return CliRootCA(hostName, rootCAFilename, eraConfig, insecureEra)
+			return cliRootCA(hostName, rootCAFilename, eraConfig, insecureEra)
 		},
 		SilenceUsage: true,
 	}
 
 	cmd.Flags().StringVarP(&rootCAFilename, "output", "o", "marblerun.crt", "File to save the certificate to")
-	cmd.Flags().StringVar(&eraConfig, "era-config", "", eraString)
+	cmd.Flags().StringVar(&eraConfig, "era-config", "", "Path to remote attestation config file in json format, if none provided the newest configuration will be loaded from github")
 	cmd.Flags().BoolVarP(&insecureEra, "insecure", "i", false, "Set to skip quote verification, needed when running in simulation mode")
 	return cmd
 }
 
-// CliRootCA gets the rootCA of the marblerun coordinator and saves it to
-func CliRootCA(host string, output string, configFilename string, insecure bool) error {
-	cert, err := VerifyCoordinator(host, configFilename, insecure)
+// cliRootCA gets the rootCA of the marblerun coordinator and saves it to a file
+func cliRootCA(host string, output string, configFilename string, insecure bool) error {
+	cert, err := verifyCoordinator(host, configFilename, insecure)
 	if err != nil {
 		return err
 	}
