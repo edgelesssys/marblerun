ff04fa807a3044c9c9c58b58d9454b65dbd0548e
daniel-weisse <daniel.weisse@gmx.net>
2021-03-25 17:17:06 +0100

    Fix seg fault on marble activation

--- coordinator/manifest/manifest.go
+++ coordinator/manifest/manifest.go
@@ -62,7 +62,11 @@ func (m Manifest) Check(ctx context.Context, zaplogger *zap.Logger) error {
 	// if len(m.Infrastructures) <= 0 {
 	// 	return errors.New("no allowed infrastructures defined")
 	// }
-	for _, marble := range m.Marbles {
+	for idx, marble := range m.Marbles {
+		if marble.Parameters == nil {
+			marble.Parameters = &rpc.Parameters{}
+			m.Marbles[idx] = marble
+		}
 		singlePackage, ok := m.Packages[marble.Package]
 		if !ok {
 			return errors.New("manifest does not contain marble package " + marble.Package)
