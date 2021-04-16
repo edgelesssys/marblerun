aed8cd28743594f87d81eaa9c3be21f1fb1f55f1
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2021-04-16 14:16:08 +0200

    AB#804 Add CLI complete command (#138)
    * AB#804 Add CLI complete command
    
    * Drop fish support for now

---
+++ cli/cmd/completion_test.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCliCompletion(t *testing.T) {
+	assert := assert.New(t)
+
+	bashCompletion, err := cliCompletion("bash", rootCmd)
+	assert.NoError(err)
+	assert.Contains(bashCompletion, "# bash completion for marblerun")
+
+	//fishCompletion, err := cliCompletion("fish", rootCmd)
+	//assert.NoError(err)
+	//assert.Contains(fishCompletion, "# fish completion for marblerun")
+
+	zshCompletion, err := cliCompletion("zsh", rootCmd)
+	assert.NoError(err)
+	assert.Contains(zshCompletion, "# zsh completion for marblerun")
+
+	_, err = cliCompletion("unsupproted-shell", rootCmd)
+	assert.Error(err)
+}
