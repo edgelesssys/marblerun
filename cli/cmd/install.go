c52f21ec0921d052f34ca10ae2d8ebb51b234025
daniel-weisse <daniel.weisse@gmx.net>
2021-04-19 11:57:31 +0200

    refactor helm chart generation

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -33,11 +33,12 @@ type installOptions struct {
 	chartPath        string
 	hostname         string
 	version          string
-	defaultKey       string
+	resourceKey      string
 	simulation       bool
 	disableInjection bool
 	clientPort       int
 	meshPort         int
+	kubeClient       kubernetes.Interface
 	settings         *cli.EnvSettings
 }
 
@@ -51,6 +52,12 @@ func newInstallCmd() *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			options.settings = cli.New()
+			var err error
+			options.kubeClient, err = getKubernetesInterface()
+			if err != nil {
+				return err
+			}
+
 			return cliInstall(options)
 		},
 		SilenceUsage: true,
@@ -59,7 +66,7 @@ func newInstallCmd() *cobra.Command {
 	cmd.Flags().StringVar(&options.hostname, "domain", "localhost", "Sets the CNAME for the coordinator certificate")
 	cmd.Flags().StringVar(&options.chartPath, "marblerun-chart-path", "", "Path to marblerun helm chart")
 	cmd.Flags().StringVar(&options.version, "version", "", "Version of the Coordinator to install, latest by default")
-	cmd.Flags().StringVar(&options.defaultKey, "resource-key", intelEpc.String(), "Resource providing SGX, different depending on used device plugin")
+	cmd.Flags().StringVar(&options.resourceKey, "resource-key", "", "Resource providing SGX, different depending on used device plugin. Use this to set tolerations/resources if your device plugin is not supported by marblerun")
 	cmd.Flags().BoolVar(&options.simulation, "simulation", false, "Set marblerun to start in simulation mode")
 	cmd.Flags().BoolVar(&options.disableInjection, "disable-auto-injection", false, "Disable automatic injection of selected namespaces")
 	cmd.Flags().IntVar(&options.meshPort, "mesh-server-port", 2001, "Set the mesh server port. Needs to be configured to the same port as in the data-plane marbles")
@@ -98,14 +105,19 @@ func cliInstall(options *installOptions) error {
 		return err
 	}
 
-	resourceKey, err := getResourceKey(options.defaultKey)
-	if err != nil {
-		return err
+	var resourceKey string
+	if len(options.resourceKey) <= 0 {
+		resourceKey, err = getSGXResourceKey(options.kubeClient)
+		if err != nil {
+			return err
+		}
+	} else {
+		resourceKey = options.resourceKey
 	}
 
 	// set overwrite values
 	finalValues := map[string]interface{}{}
-	var stringValues []string
+	stringValues := []string{}
 
 	stringValues = append(stringValues, fmt.Sprintf("coordinator.meshServerPort=%d", options.meshPort))
 	stringValues = append(stringValues, fmt.Sprintf("coordinator.clientServerPort=%d", options.clientPort))
@@ -120,51 +132,71 @@ func cliInstall(options *installOptions) error {
 		)
 	} else {
 		stringValues = append(stringValues,
-			fmt.Sprintf("tolerations[0].key=%s", resourceKey),
-			fmt.Sprintf("tolerations[0].operator=Exists"),
-			fmt.Sprintf("tolerations[0].effect=NoSchedule"),
 			fmt.Sprintf("coordinator.hostname=%s", options.hostname),
 		)
+
+		// Helms value merge function will overwrite any preset values for "tolerations" if we set new ones here
+		// To avoid this we set the new toleration for "resourceKey" and copy all preset tolerations
+		needToleration := true
+		idx := 0
+		for _, toleration := range chart.Values["tolerations"].([]interface{}) {
+			if key, ok := toleration.(map[string]interface{})["key"]; ok {
+				if key == resourceKey {
+					needToleration = false
+				}
+				stringValues = append(stringValues, fmt.Sprintf("tolerations[%d].key=%v", idx, key))
+			}
+			if operator, ok := toleration.(map[string]interface{})["operator"]; ok {
+				stringValues = append(stringValues, fmt.Sprintf("tolerations[%d].operator=%v", idx, operator))
+			}
+			if effect, ok := toleration.(map[string]interface{})["effect"]; ok {
+				stringValues = append(stringValues, fmt.Sprintf("tolerations[%d].effect=%v", idx, effect))
+			}
+			if value, ok := toleration.(map[string]interface{})["value"]; ok {
+				stringValues = append(stringValues, fmt.Sprintf("tolerations[%d].value=%v", idx, value))
+			}
+			if tolerationSeconds, ok := toleration.(map[string]interface{})["tolerationSeconds"]; ok {
+				stringValues = append(stringValues, fmt.Sprintf("tolerations[%d].tolerationSeconds=%v", idx, tolerationSeconds))
+			}
+			idx++
+		}
+		if needToleration {
+			stringValues = append(stringValues,
+				fmt.Sprintf("tolerations[%d].key=%s", idx, resourceKey),
+				fmt.Sprintf("tolerations[%d].operator=Exists", idx),
+				fmt.Sprintf("tolerations[%d].effect=NoSchedule", idx),
+			)
+		}
+	}
+
+	if !options.disableInjection {
+		injectorValues, err := installWebhook(options.kubeClient)
+		if err != nil {
+			return errorAndCleanup(err, options.kubeClient)
+		}
+
+		for _, val := range injectorValues {
+			stringValues = append(stringValues, val)
+		}
+		stringValues = append(stringValues, fmt.Sprintf("marbleInjector.resourceKey=%s", resourceKey))
 	}
 
 	for _, val := range stringValues {
 		if err := strvals.ParseInto(val, finalValues); err != nil {
-			return err
+			return errorAndCleanup(err, options.kubeClient)
 		}
 	}
 
-	// strvals cant parse keys which include dots, e.g. setting as a resource limit key "sgx.intel.com/epc" will lead to errors
-	// With this we directly set the needed values as a map[string]interface
 	if !options.simulation {
-		// Since we want to potentially change a key in the "limits" map we have to remove keys that are different from the one we want to insert
-		// If we just set a new key the resulting deployment will potentially have 2 resource limits for different SGX definitions resulting in a pod that will never start
-		if presetLimits, ok := chart.Values["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{}); ok {
-			finalValues["coordinator"].(map[string]interface{})["resources"] = map[string]interface{}{
-				"limits": map[string]interface{}{},
-			}
-			for oldResourceKey := range presetLimits {
-				// Make sure the key we delete is not a common kubernetes resource type
-				if oldResourceKey != resourceKey && !(strings.Contains("cpu memory", oldResourceKey) || strings.Contains(oldResourceKey, "hugepages-")) {
-					finalValues["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{})[oldResourceKey] = nil
-				}
-			}
-		}
-		// Set the new sgx resource limit
-		finalValues["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{})[resourceKey] = 10
-	}
-
-	if !options.disableInjection {
-		if err := installWebhook(finalValues, resourceKey); err != nil {
-			return errorAndCleanup(err)
-		}
+		setSGXValues(resourceKey, finalValues, chart.Values)
 	}
 
 	if err := chartutil.ValidateAgainstSchema(chart, finalValues); err != nil {
-		return errorAndCleanup(err)
+		return errorAndCleanup(err, options.kubeClient)
 	}
 
 	if _, err := installer.Run(chart, finalValues); err != nil {
-		return errorAndCleanup(err)
+		return errorAndCleanup(err, options.kubeClient)
 	}
 
 	fmt.Println("Marblerun installed successfully")
@@ -227,44 +259,40 @@ func getRepo(name string, url string, settings *cli.EnvSettings) error {
 }
 
 // installWebhook enables a mutating admission webhook to allow automatic injection of values into pods
-func installWebhook(vals map[string]interface{}, resourceKey string) error {
-	kubeClient, err := getKubernetesInterface()
-	if err != nil {
-		return err
-	}
-
+func installWebhook(kubeClient kubernetes.Interface) ([]string, error) {
 	// verify marblerun namespace exists, if not create it
 	if err := verifyNamespace("marblerun", kubeClient); err != nil {
-		return err
+		return nil, err
 	}
 
 	fmt.Printf("Setting up Marblerun Webhook")
 	certificateHandler, err := getCertificateHandler(kubeClient)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	fmt.Printf(".")
 	if err := certificateHandler.signRequest(); err != nil {
-		return err
+		return nil, err
 	}
 	fmt.Printf(".")
-	if err := certificateHandler.setCaBundle(vals, resourceKey); err != nil {
-		return err
+	injectorValues, err := certificateHandler.setCaBundle()
+	if err != nil {
+		return nil, err
 	}
 	cert, err := certificateHandler.get()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if len(cert) <= 0 {
-		return fmt.Errorf("certificate was not signed by the CA")
+		return nil, fmt.Errorf("certificate was not signed by the CA")
 	}
 	fmt.Printf(".")
 
 	if err := createSecret(certificateHandler.getKey(), cert, kubeClient); err != nil {
-		return err
+		return nil, err
 	}
 	fmt.Printf(" Done\n")
-	return nil
+	return injectorValues, nil
 }
 
 // createSecret creates a secret containing the signed certificate and private key for the webhook server
@@ -334,13 +362,8 @@ func verifyNamespace(namespace string, kubeClient kubernetes.Interface) error {
 	return nil
 }
 
-// getResourceKey checks what device plugin is providing SGX on the cluster and returns the corresponding resource key
-func getResourceKey(defaultKey string) (string, error) {
-	kubeClient, err := getKubernetesInterface()
-	if err != nil {
-		return "", err
-	}
-
+// getSGXResourceKey checks what device plugin is providing SGX on the cluster and returns the corresponding resource key
+func getSGXResourceKey(kubeClient kubernetes.Interface) (string, error) {
 	nodes, err := kubeClient.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return "", err
@@ -356,19 +379,51 @@ func getResourceKey(defaultKey string) (string, error) {
 	}
 
 	// assume cluster has the intel SGX device plugin by default
-	return defaultKey, nil
+	return intelEpc.String(), nil
+}
+
+// setSGXValues sets the needed values for the coorindator as a map[string]interface
+// strvals cant parse keys which include dots, e.g. setting as a resource limit key "sgx.intel.com/epc" will lead to errors
+func setSGXValues(resourceKey string, values, chartValues map[string]interface{}) {
+	values["coordinator"].(map[string]interface{})["resources"] = map[string]interface{}{
+		"limits":   map[string]interface{}{},
+		"requests": map[string]interface{}{},
+	}
+
+	toRemove := fmt.Sprintf("%s %s", intelEpc.String(), azureEpc.String())
+	var needNewLimit bool
+
+	// remove all previously set sgx resource limits
+	if presetLimits, ok := chartValues["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{}); ok {
+		for oldResourceKey := range presetLimits {
+			// Make sure the key we delete is an unwanted sgx resource and not a custom resource or common resource (cpu, memory, etc.)
+			if oldResourceKey != resourceKey && strings.Contains(toRemove, oldResourceKey) {
+				values["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{})[oldResourceKey] = nil
+				needNewLimit = true
+			}
+		}
+	}
+
+	// remove all previously set sgx resource requests
+	if presetLimits, ok := chartValues["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["requests"].(map[string]interface{}); ok {
+		for oldResourceKey := range presetLimits {
+			if oldResourceKey != resourceKey && strings.Contains(toRemove, oldResourceKey) {
+				values["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["requests"].(map[string]interface{})[oldResourceKey] = nil
+				needNewLimit = true
+			}
+		}
+	}
+
+	// Set the new sgx resource limit, kubernetes will automatically set a resource request equal to the limit
+	if needNewLimit {
+		values["coordinator"].(map[string]interface{})["resources"].(map[string]interface{})["limits"].(map[string]interface{})[resourceKey] = 10
+	}
 }
 
 // errorAndCleanup returns the given error and deletes resources which might have been created previously
 // This prevents secrets and CSRs to stay on the cluster after a failed installation attempt
-func errorAndCleanup(err error) error {
-	// If we cant even create a clientset wed dont attempt to remove any resources
-	kubeClient, kubeErr := getKubernetesInterface()
-	if kubeErr != nil {
-		return err
-	}
-
-	// We dont care about any errors here
+func errorAndCleanup(err error, kubeClient kubernetes.Interface) error {
+	// We dont care about any additional errors here
 	cleanupCSR(kubeClient)
 	cleanupSecrets(kubeClient)
 	return err
--- cli/cmd/install_test.go
+++ cli/cmd/install_test.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"crypto/rand"
 	"crypto/rsa"
+	"errors"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	certv1 "k8s.io/api/certificates/v1"
 	corev1 "k8s.io/api/core/v1"
+	kubeErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/version"
 	fakediscovery "k8s.io/client-go/discovery/fake"
@@ -81,4 +85,86 @@ func TestVerifyNamespace(t *testing.T) {
 	// namespace exists, should return nil
 	err = verifyNamespace("test-space", testClient)
 	require.NoError(err)
+}
+
+func TestInstallWebhook(t *testing.T) {
+	assert := assert.New(t)
+
+	testClient := fake.NewSimpleClientset()
+	testClient.Discovery().(*fakediscovery.FakeDiscovery).FakedServerVersion = &version.Info{
+		Major: "1",
+		Minor: "18",
+	}
+
+	testValues, err := installWebhook(testClient)
+	assert.NoError(err)
+	assert.Equal("marbleInjector.start=true", testValues[0], "failed to set start to true")
+	assert.Contains(testValues[1], "LS0t", "failed to set CABundle")
+}
+
+func TestGetSGXResourceKey(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testClient := fake.NewSimpleClientset()
+
+	// Test Intel Device Plugin
+	intelSGXNode := &corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "intel-sgx-node",
+		},
+		Status: corev1.NodeStatus{
+			Capacity: corev1.ResourceList{
+				intelEnclave:   resource.MustParse("10"),
+				intelEpc:       resource.MustParse("500"),
+				intelProvision: resource.MustParse("10"),
+			},
+		},
+	}
+	_, err := testClient.CoreV1().Nodes().Create(context.TODO(), intelSGXNode, metav1.CreateOptions{})
+	require.NoError(err)
+
+	resourceKey, err := getSGXResourceKey(testClient)
+	assert.NoError(err)
+	assert.Equal(intelEpc.String(), resourceKey)
+}
+
+func TestErrorAndCleanup(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testClient := fake.NewSimpleClientset()
+	testClient.Discovery().(*fakediscovery.FakeDiscovery).FakedServerVersion = &version.Info{
+		Major: "1",
+		Minor: "19",
+	}
+
+	testError := errors.New("test")
+	err := errorAndCleanup(testError, testClient)
+	assert.Equal(testError, err)
+
+	// Create and test for CSR
+	csr := &certv1.CertificateSigningRequest{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: webhookName,
+		},
+		Spec: certv1.CertificateSigningRequestSpec{
+			Request:    []byte{0xAA, 0xAA, 0xAA},
+			SignerName: "kubernetes.io/kubelet-serving",
+			Usages: []certv1.KeyUsage{
+				"key encipherment", "digital signature", "server auth",
+			},
+		},
+	}
+
+	_, err = testClient.CertificatesV1().CertificateSigningRequests().Create(context.TODO(), csr, metav1.CreateOptions{})
+	require.NoError(err)
+
+	_, err = testClient.CertificatesV1().CertificateSigningRequests().Get(context.TODO(), webhookName, metav1.GetOptions{})
+
+	err = errorAndCleanup(testError, testClient)
+	assert.Equal(testError, err)
+
+	_, err = testClient.CertificatesV1().CertificateSigningRequests().Get(context.TODO(), webhookName, metav1.GetOptions{})
+	assert.True(kubeErrors.IsNotFound(err))
 }
