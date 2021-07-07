50fe1edb4d9ef22a5bf309318a4388ef9ccceed4
daniel-weisse <daniel.weisse@gmx.net>
2021-07-07 16:01:15 +0200

    fix missing prometheus parameter

--- coordinator/core/openssl_test.go
+++ coordinator/core/openssl_test.go
@@ -52,7 +52,7 @@ func TestOpenSSLVerify(t *testing.T) {
 	issuer := quote.NewMockIssuer()
 	sealer := &seal.MockSealer{}
 	recovery := recovery.NewSinglePartyRecovery()
-	coreServer, err := NewCore([]string{"localhost"}, validator, issuer, sealer, recovery, zapLogger)
+	coreServer, err := NewCore([]string{"localhost"}, validator, issuer, sealer, recovery, zapLogger, nil)
 	require.NoError(err)
 	require.NotNil(coreServer)
 
