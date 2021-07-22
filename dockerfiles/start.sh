6d34bbf28e208b00b490bdef4cdabb485fc62460
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2021-07-22 11:48:13 +0200

    AB#1088 Set dcap library on docker container start (#221)

---
+++ dockerfiles/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "${DCAP_LIBRARY}" == "intel" ]]
+then
+    # rename the library installed by az-dcap-client
+    mv /usr/lib/libdcap_quoteprov.so /usr/lib/libdcap_quoteprov.so.azure
+    # create a link to the intel quote provider library
+    mv /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1.intel /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1
+    ln -s /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1 /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so
+fi
+
+erthost coordinator-enclave.signed
