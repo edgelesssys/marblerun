f8a4e6a84126c46948425bd464c9303e69aca0ac
Nils Hanke <nils.hanke@rub.de>
2021-07-26 13:35:09 +0000

    get/put wip

--- coordinator/store/akvstore.go
+++ coordinator/store/akvstore.go
@@ -7,6 +7,9 @@
 package store
 
 import (
+	"context"
+	"encoding/base64"
+	"net/http"
 	"strings"
 	"sync"
 
@@ -19,6 +22,7 @@ type AKVStore struct {
 	akvClient  keyvault.BaseClient
 	baseUrl    string
 	mux, txmux sync.Mutex
+	ctx        context.Context
 }
 
 func NewAKVStore(vaultUrl string) (*AKVStore, error) {
@@ -32,19 +36,36 @@ func NewAKVStore(vaultUrl string) (*AKVStore, error) {
 	s := &AKVStore{
 		akvClient: client,
 		baseUrl:   vaultUrl,
+		ctx:       context.TODO(), // TODO: Find a proper context here
 	}
 	return s, nil
 }
 
 // Get retrieves a value from the keyvault
 func (s *AKVStore) Get(request string) ([]byte, error) {
-	// request a value using s.akvClient.GetSecret
-	return nil, nil
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	result, err := s.akvClient.GetSecret(s.ctx, s.baseUrl, request, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return base64.RawStdEncoding.DecodeString(*result.Value)
 }
 
 // Put saves a values to the keyvault
 func (s *AKVStore) Put(request string, requestData []byte) error {
-	// save a value using s.akvClient.SetSecret
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	requestDataBase64 := base64.RawStdEncoding.EncodeToString(requestData)
+	secretParameters := keyvault.SecretSetParameters{
+		Value: &requestDataBase64,
+	}
+	_, err := s.akvClient.SetSecret(s.ctx, s.baseUrl, request, secretParameters)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -58,7 +79,7 @@ func (s *AKVStore) Iterator(prefix string) (Iterator, error) {
 
 // BeginTransaction starts a new transaction for the keyvault
 func (s *AKVStore) BeginTransaction() (Transaction, error) {
-	tx := &akvTransaction{store: s, data: map[string]byte{}}
+	tx := &akvTransaction{store: s, data: map[string]akvTransactionData{}}
 	s.txmux.Lock()
 	return tx, nil
 }
@@ -71,16 +92,34 @@ func (s *AKVStore) LoadState() ([]byte, error) {
 
 type akvTransaction struct {
 	store *AKVStore
-	data  map[string]byte //maybe use a different structure here? Potential option to make use of GetSecretPreparer
+	data  map[string]akvTransactionData //maybe use a different structure here? Potential option to make use of GetSecretPreparer
+}
+
+type akvTransactionData struct {
+	data    []byte
+	request *http.Request
 }
 
 func (t *akvTransaction) Get(request string) ([]byte, error) {
-	// return a value from the transaction
-	return nil, nil
+	if value, ok := t.data[request]; ok {
+		return value.data, nil
+	}
+	return nil, &storeValueUnset{requestedValue: request}
 }
 
 func (t *akvTransaction) Put(request string, requestData []byte) error {
-	// add a value to the transaction
+	requestDataBase64 := base64.RawStdEncoding.EncodeToString(requestData)
+	secretParameters := keyvault.SecretSetParameters{
+		Value: &requestDataBase64,
+	}
+	preparedRequest, err := t.store.akvClient.SetSecretPreparer(t.store.ctx, t.store.baseUrl, request, secretParameters)
+	if err != nil {
+		return err
+	}
+
+	singleTransaction := akvTransactionData{data: requestData, request: preparedRequest}
+	t.data[request] = singleTransaction
+
 	return nil
 }
 
@@ -96,7 +135,17 @@ func (t *akvTransaction) Iterator(prefix string) (Iterator, error) {
 }
 
 func (t *akvTransaction) Commit() error {
-	// save all values from the transaction to the keyvault
+	// TODO: Wrap this in a goroutine for parallelized setting?
+	for _, value := range t.data {
+		resp, err := t.store.akvClient.SetSecretSender(value.request)
+		if err != nil {
+			return err
+		}
+		_, err = t.store.akvClient.SetSecretResponder(resp)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
