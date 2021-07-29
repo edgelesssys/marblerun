4783558941ed4eab997d53fba5c4b859e2071491
daniel-weisse <daniel.weisse@gmx.net>
2021-07-29 13:43:30 +0200

    Set az-dcap-client log level on start

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -7,6 +7,8 @@ then
     # create a link to the intel quote provider library
     mv /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1.intel /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1
     ln -s /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1 /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so
+else
+    export AZDCAP_DEBUG_LOG_LEVEL="${AZDCAP_DEBUG_LOG_LEVEL:=ERROR}"
 fi
 
 erthost coordinator-enclave.signed
