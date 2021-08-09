044311badc00719ca52560d9c55d76aa8e8667f7
Daniel Weiße <daniel.weisse@gmx.net>
2021-08-09 11:08:41 +0000

    create intel qpl symlinks from subfolder

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -5,8 +5,8 @@ then
     # rename the library installed by az-dcap-client
     mv /usr/lib/libdcap_quoteprov.so /usr/lib/libdcap_quoteprov.so.azure
     # create a link to the intel quote provider library
-    mv /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1.intel /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1
-    ln -s /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1 /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so
+    ln -s /usr/lib/x86_64-linux-gnu/dcap/libdcap_quoteprov.so.intel /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so
+    ln -s /usr/lib/x86_64-linux-gnu/dcap/libdcap_quoteprov.so.intel /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1
 else
     export AZDCAP_DEBUG_LOG_LEVEL="${AZDCAP_DEBUG_LOG_LEVEL:=ERROR}"
 fi
