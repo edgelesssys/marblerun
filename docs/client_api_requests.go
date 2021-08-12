e405e687db5c3372638d19fb5ca5c7f87bcf05ec
katexochen <paul.meyer-y2x@ruhr-uni-bochum.de>
2021-08-12 12:33:25 +0000

    delete dummy model

--- docs/client_api_requests.go
+++ docs/client_api_requests.go
@@ -17,19 +17,3 @@ type ManifestPostRequest struct {
 	// in:body
 	Manifest manifest.Manifest
 }
-
-// Userers This is the start of the description This is the start of the description This is the start of the description
-// User represents the user for this application This is the start of the description
-// swagger:model
-type Userers struct {
-	// the id for this user
-	//
-	// required: true
-	// min: 1
-	ID int64 `json:"id"`
-
-	// the name for this user
-	// required: true
-	// min length: 3
-	Name string `json:"name"`
-}
