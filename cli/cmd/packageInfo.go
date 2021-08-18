0c04839fdebdb0c68d123c75f23fa0a482bc545a
Nils Hanke <nils.hanke@rub.de>
2021-08-18 17:23:27 +0200

    Add support for decoding Graphene SIGSTRUCT

--- cli/cmd/packageInfo.go
+++ cli/cmd/packageInfo.go
@@ -8,6 +8,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -15,16 +16,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newSGXSDKPackageInfoCmd() *cobra.Command {
+func newPackageInfoCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sgxsdk-package-info",
-		Short: "Prints the package signature properties of an SGX SDK binary",
-		Long:  "Prints the package signature properties of an SGX SDK binary",
-		Args:  cobra.ExactArgs(1),
+		Use:   "package-info",
+		Short: "Prints the package signature properties of an enclave",
+		Long:  "Prints the package signature properties of an enclave",
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			path := args[0]
+			enclaveType := args[0]
+			path := args[1]
 
-			return decodeSigStruct(path)
+			if enclaveType == "occlum" || enclaveType == "sgxsdk" {
+				return decodeSGXSDKSigStruct(path)
+			} else if enclaveType == "graphene" {
+				return decodeGrapheneSigStruct(path)
+			}
+
+			return errors.New("unsupported enclave type")
 		},
 		SilenceUsage: true,
 	}
@@ -32,7 +40,7 @@ func newSGXSDKPackageInfoCmd() *cobra.Command {
 	return cmd
 }
 
-func decodeSigStruct(path string) error {
+func decodeSGXSDKSigStruct(path string) error {
 	// Check if given filename is actually a directory
 	stat, err := os.Stat(path)
 	if err != nil {
@@ -82,10 +90,8 @@ func decodeSigStruct(path string) error {
 	} else {
 		color.Cyan("PackageProperties for '%s':\n", path)
 	}
-	fmt.Printf("UniqueID (MRENCLAVE)      : %s\n", hex.EncodeToString(mrenclave))
-	fmt.Printf("SignerID (MRSIGNER)       : %s\n", hex.EncodeToString(mrsigner[:]))
-	fmt.Printf("ProductID (ISVPRODID)     : %d\n", binary.LittleEndian.Uint16(isvprodid))
-	fmt.Printf("SecurityVersion (ISVSVN)  : %d\n", binary.LittleEndian.Uint16(isvsvn))
+
+	printPackageProperties(mrenclave, mrsigner, isvprodid, isvsvn)
 
 	return nil
 }
@@ -130,4 +136,69 @@ func parseSigStruct(sgxMetaData []byte) ([]byte, []byte, []byte, []byte, error) 
 	mrsigner := sha256.Sum256(modulus)
 
 	return mrenclave, mrsigner[:], isvprodid, isvsvn, nil
+}
+
+func decodeGrapheneSigStruct(path string) error {
+	// Check if given filename is actually a directory
+	stat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	// Check if directory contains a file ending in .sig
+	var sigFile string
+	var isDirectory bool
+	if isDirectory = stat.IsDir(); isDirectory {
+		fsInfo, err := ioutil.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		foundSigFile := false
+		for _, entry := range fsInfo {
+			if filepath.Ext(entry.Name()) == ".sig" {
+				if foundSigFile {
+					return errors.New("found multiple .sig files")
+				}
+				foundSigFile = true
+				sigFile = entry.Name()
+				color.Green("Detected Graphene instance.")
+			}
+		}
+		if !foundSigFile {
+			color.Red("ERROR: A directory was supplied, but a Graphene SGX signature file cannot be found.")
+			color.Red("Please either specify a correct directory, or point to the Graphene SGX .sig file directly.")
+			return errors.New("did not find Graphene .sig file in directory")
+		}
+		sigFile = filepath.Join(path, sigFile)
+	} else {
+		sigFile = path
+	}
+
+	// Try to load the file
+	sigContent, err := ioutil.ReadFile(sigFile)
+	if err != nil {
+		return err
+	}
+
+	mrenclave, mrsigner, isvprodid, isvsvn, err := parseSigStruct(sigContent)
+	if err != nil {
+		return err
+	}
+
+	if isDirectory {
+		color.Cyan("PackageProperties for Graphene instance at '%s':\n", path)
+	} else {
+		color.Cyan("PackageProperties for '%s':\n", path)
+	}
+
+	printPackageProperties(mrenclave, mrsigner[:], isvprodid, isvsvn)
+
+	return nil
+}
+
+func printPackageProperties(mrenclave []byte, mrsigner []byte, isvprodid []byte, isvsvn []byte) {
+	fmt.Printf("UniqueID (MRENCLAVE)      : %s\n", hex.EncodeToString(mrenclave))
+	fmt.Printf("SignerID (MRSIGNER)       : %s\n", hex.EncodeToString(mrsigner[:]))
+	fmt.Printf("ProductID (ISVPRODID)     : %d\n", binary.LittleEndian.Uint16(isvprodid))
+	fmt.Printf("SecurityVersion (ISVSVN)  : %d\n", binary.LittleEndian.Uint16(isvsvn))
 }
