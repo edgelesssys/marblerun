be714ce1199dc70e3caa6c99b4fac1e3277c0913
Thomas Tendyck <51411342+thomasten@users.noreply.github.com>
2023-01-31 10:00:59 +0100

    Apply suggestions from code review
    Co-authored-by: Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -2,9 +2,9 @@
 
 if [[ "${DCAP_LIBRARY}" == "intel" ]]
 then
-    apt-get install -qq libsgx-dcap-default-qpl > /dev/null
+    apt-get install -qq libsgx-dcap-default-qpl > /dev/null 2>&1
 else
-    apt-get install -qq az-dcap-client > /dev/null
+    apt-get install -qq az-dcap-client > /dev/null 2>&1
 fi
 
 erthost coordinator-enclave.signed
