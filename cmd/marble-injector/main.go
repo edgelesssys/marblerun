e15a0ea7a64d78bd92700e36b127f35e89783806
orion <szilard.parrag@gmail.com>
2023-02-20 09:03:05 +0100

    injector, cli: followup for {cert,key}.pem -> tls.{crt,key} name changes
    Signed-off-by: OverOrion <szilard.parrag@gmail.com>

--- cmd/marble-injector/main.go
+++ cmd/marble-injector/main.go
@@ -21,8 +21,8 @@ func main() {
 	var clusterDomain string
 	var sgxResource string
 	flag.StringVar(&addr, "coordAddr", "coordinator-mesh-api.marblerun:2001", "Address of the MarbleRun coordinator")
-	flag.StringVar(&certFile, "tlsCertFile", "/etc/webhook/certs/cert.pem", "File containing the x509 Certificate for HTTPS.")
-	flag.StringVar(&keyFile, "tlsKeyFile", "/etc/webhook/certs/key.pem", "File containing the x509 private key to --tlsCertFile.")
+	flag.StringVar(&certFile, "tlsCertFile", "/etc/webhook/certs/tls.crt", "File containing the x509 Certificate for HTTPS.")
+	flag.StringVar(&keyFile, "tlsKeyFile", "/etc/webhook/certs/tls.key", "File containing the x509 private key to --tlsCertFile.")
 	flag.StringVar(&clusterDomain, "clusterDomain", "cluster.local", "Domain name of the kubernetes cluster")
 	flag.StringVar(&sgxResource, "sgxResource", "sgx.intel.com/epc", "Defines the resource/toleration to inject, this needs to be exposed on a node through a device plugin")
 
