7bf804df5f9fd4f0e56d7d6dbc40a0a901c4559d
Thomas Tendyck <tt@edgeless.systems>
2023-06-28 17:48:28 +0200

    cli: encode PAT so that users don't have to do it manually

--- cli/internal/helm/client_test.go
+++ cli/internal/helm/client_test.go
@@ -7,10 +7,12 @@
 package helm
 
 import (
+	"encoding/base64"
 	"testing"
 
 	"github.com/edgelesssys/marblerun/util/k8sutil"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNeedsDeletion(t *testing.T) {
@@ -84,6 +86,68 @@ func TestNeedsDeletion(t *testing.T) {
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			assert.Equal(t, tc.wantDeletion, needsDeletion(tc.existingKey, tc.sgxKey))
+		})
+	}
+}
+
+func TestUpdateValues(t *testing.T) {
+	testCases := map[string]struct {
+		opts      Options
+		chartVals map[string]interface{}
+		wantVals  func(*testing.T, map[string]interface{})
+		wantErr   bool
+	}{
+		"github PAT gets encoded": {
+			opts: Options{
+				SimulationMode: true,
+				AccessToken:    "ghp_foo",
+			},
+			chartVals: map[string]interface{}{
+				"coordinator": map[string]interface{}{
+					"repository": "myrepo",
+				},
+			},
+			wantVals: func(t *testing.T, vals map[string]interface{}) {
+				assert := assert.New(t)
+				require := require.New(t)
+				pullSecret, err := base64.StdEncoding.DecodeString(vals["pullSecret"].(map[string]interface{})["token"].(string))
+				require.NoError(err)
+				assert.EqualValues(`{"auths":{"myrepo":{"auth":"Z2hwX2ZvbzpnaHBfZm9v"}}}`, pullSecret)
+			},
+		},
+		"other token is taken unmodified": {
+			opts: Options{
+				SimulationMode: true,
+				AccessToken:    "foo",
+			},
+			chartVals: map[string]interface{}{
+				"coordinator": map[string]interface{}{
+					"repository": "myrepo",
+				},
+			},
+			wantVals: func(t *testing.T, vals map[string]interface{}) {
+				assert := assert.New(t)
+				require := require.New(t)
+				pullSecret, err := base64.StdEncoding.DecodeString(vals["pullSecret"].(map[string]interface{})["token"].(string))
+				require.NoError(err)
+				assert.EqualValues(`{"auths":{"myrepo":{"auth":"foo"}}}`, pullSecret)
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			gotVals, err := UpdateValues(tc.opts, tc.chartVals)
+			if tc.wantErr {
+				assert.Error(err)
+				return
+			}
+			require.NoError(err)
+
+			tc.wantVals(t, gotVals)
 		})
 	}
 }
