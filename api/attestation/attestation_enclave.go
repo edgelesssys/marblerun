611389302ec8363b27d2322815dc39065223f5a6
Daniel Weiße <dw@edgeless.systems>
2024-05-16 10:46:11 +0200

    Move build tags to not break license header check
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- api/attestation/attestation_enclave.go
+++ api/attestation/attestation_enclave.go
@@ -1,10 +1,10 @@
-//go:build marblerun_ego_enclave
-
 // Copyright (c) Edgeless Systems GmbH.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//go:build marblerun_ego_enclave
 
 package attestation
 
