08736b9eba7ee6ce51139fa242bb015f7aa550f1
Thomas Tendyck <tt@edgeless.systems>
2024-10-07 13:44:20 +0000

    coordinator: add Marble type as extension to Marble certificate

---
+++ coordinator/oid/oid.go
@@ -0,0 +1,12 @@
+// Copyright (c) Edgeless Systems GmbH.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package oid
+
+import "encoding/asn1"
+
+// MarbleType is the OID for the Marble type in the Marble certificate.
+var MarbleType = asn1.ObjectIdentifier{1, 3, 9903, 2, 1}
